235e7b215e5a52bf5c248df212a5a245e0365de9
Patrick Lynch <pajlynch@gmail.com>
2021-05-04 22:39:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,19 @@
 class Book
- 
+
   def initialize(title)
     @title = title
   end
- 
+
   def title
     @title
   end
- 
+
   def author=(author)
     @author = author
   end
- 
+
   def author
     @author
   end
- 
+
 end
